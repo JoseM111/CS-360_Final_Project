fa9eb18ce5781070cbef2ac4c8ca7489f2a8654e
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2023-02-08 15:03:09 -0500

    module-5: completed Msg view

--- app/src/main/java/com/josem111/ui/msg/MsgFragment.java
+++ app/src/main/java/com/josem111/ui/msg/MsgFragment.java
@@ -1,68 +1,50 @@
 package com.josem111.ui.msg;
 
+import static com.josem111.ui.msg.PermissionManager.checkPermission;
+
 import android.Manifest;
-import android.content.pm.PackageManager;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.Toast;
 
 import com.josem111.R;
 
-import java.util.Objects;
 
 public class MsgFragment extends Fragment {
-    private static final int SMS_PERMISSION_REQUEST_CODE = 1;
-    private MsgViewModel mViewModel;
 
-    public static MsgFragment newInstance() {
-        return new MsgFragment();
+    public MsgFragment() {
+        // Required empty public constructor
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        mViewModel = new MsgViewModel();
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_msg, container, false);
 
-        Button mButton = view.findViewById(R.id.button_request_permission);
-        mButton.setOnClickListener((View v) -> requestPermission());
+        Button showPopupBtn = view.findViewById(R.id.show_popup_btn);
+        showPopupBtn.setOnClickListener((View v) -> showPopup());
 
         return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-    }
-
-    private void requestPermission() {
-        if (ContextCompat.checkSelfPermission(Objects.requireNonNull(getContext()),
-                Manifest.permission.SEND_SMS) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.SEND_SMS}, SMS_PERMISSION_REQUEST_CODE);
-        } else {
-            Toast.makeText(getContext(), "Permission already granted", Toast.LENGTH_SHORT).show();
-            mViewModel.setPermissionGranted(true);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (requestCode == SMS_PERMISSION_REQUEST_CODE) {
-            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Toast.makeText(getContext(), "Permission granted", Toast.LENGTH_SHORT).show();
-                mViewModel.setPermissionGranted(true);
-                // Show appropriate notifications based on user choice here
-            } else {
-                Toast.makeText(getContext(), "Permission denied", Toast.LENGTH_SHORT).show();
-                mViewModel.setPermissionGranted(false);
-            }
-        }
+    private void showPopup() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+        builder.setTitle("Allow Text Messaging App?");
+        builder.setMessage("The app needs to communicate with the text messaging app.");
+        builder.setPositiveButton(
+                "Allow",
+                (dialog, which) -> checkPermission(getContext(),
+                        Manifest.permission.SEND_SMS
+                )
+        );
+        builder.setNegativeButton("Deny", (dialog, which) -> dialog.dismiss());
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
+
